8c053ee0383d1cb4a57161a7935a59ff98644f53
Henrique-182 <henriqueaugustolobo.dev@gmail.com>
2023-01-02 09:35:08 -0300

    Lógica da Calculadora #04
    Melhorando a lógica

--- calculadora/src/br/com/cod3r/calculadora/modelo/Memoria.java
+++ calculadora/src/br/com/cod3r/calculadora/modelo/Memoria.java
@@ -13,7 +13,10 @@ public class Memoria {
 	
 	private final List<MemoriaObservador> observadores = new ArrayList<>(); 
 	
+	private TipoComando ultimaOperacao = null;
+	private boolean substituir = false;
 	private String textoAtual = "";
+	private String textoBuffer = "";
 
 	private Memoria() {
 		
@@ -34,10 +37,18 @@ public class Memoria {
 	public void processarComando(String texto) {
 		TipoComando tipoComando = detectarTipoComando(texto);
 		
-		if ("AC".equals(texto)) {
+		if (tipoComando == null) {
+			return;
+		} else if(tipoComando == TipoComando.ZERAR) {
 			textoAtual = "";
+			textoBuffer = "";
+			substituir = false;
+			ultimaOperacao = null;
+		} else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
+			textoAtual = substituir ? texto : textoAtual+texto;
+			substituir = false;
 		} else {
-			textoAtual += texto;			
+			
 		}
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
 	}
@@ -64,7 +75,7 @@ public class Memoria {
 				return TipoComando.SUBTRACAO;
 			} else if("=".equals(texto)) {
 				return TipoComando.IGUAL;
-			} else if(",".equals(texto)) {
+			} else if(",".equals(texto) && !textoAtual.contains(",")) {
 				return TipoComando.VIRGULA;
 			} 
 		}
