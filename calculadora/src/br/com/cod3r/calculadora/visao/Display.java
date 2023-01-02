6fe657074ed43d303bfce436c2388ca22472a17b
Henrique-182 <henriqueaugustolobo.dev@gmail.com>
2023-01-02 07:39:25 -0300

    Componente Display

--- calculadora/src/br/com/cod3r/calculadora/visao/Display.java
+++ calculadora/src/br/com/cod3r/calculadora/visao/Display.java
@@ -1,11 +1,25 @@
 package br.com.cod3r.calculadora.visao;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel {
 
+	private final JLabel label;
+	
 	public Display() {
+		setBackground(new Color(46, 49, 50));
+		label = new JLabel("123456");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
 		
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 22));
+		
+		add(label);
 	}
 }
