95b817338d70b187b1bc4abea16779c5359dab73
Henrique-182 <henriqueaugustolobo.dev@gmail.com>
2023-01-03 06:56:28 -0300

    Lógica da Calculadora #05
    Adicionando resultado das operações

--- calculadora/src/br/com/cod3r/calculadora/modelo/Memoria.java
+++ calculadora/src/br/com/cod3r/calculadora/modelo/Memoria.java
@@ -48,9 +48,37 @@ public class Memoria {
 			textoAtual = substituir ? texto : textoAtual+texto;
 			substituir = false;
 		} else {
-			
+			substituir = true;		
+			textoAtual = obterResultadoOperacao();
+			textoBuffer = textoAtual;
+			ultimaOperacao = tipoComando;
 		}
 		observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
+	}
+
+	private String obterResultadoOperacao() {
+		if (ultimaOperacao == null) {
+			return textoAtual;
+		}
+		double numeroBuffer = Double.parseDouble(textoBuffer.replace(",", "."));
+		double numeroAtual = Double.parseDouble(textoAtual.replace(",", "."));
+		
+		double resultado = 0;
+		
+		if (ultimaOperacao == TipoComando.SOMA) {
+			resultado = numeroBuffer + numeroAtual;
+		} else if (ultimaOperacao == TipoComando.SUBTRACAO) {
+			resultado = numeroBuffer - numeroAtual;
+		} else if (ultimaOperacao == TipoComando.MULTIPLICACAO) {
+			resultado = numeroBuffer * numeroAtual;
+		} else if (ultimaOperacao == TipoComando.DIVISAO) {
+			resultado = numeroBuffer / numeroAtual;
+		}
+		
+		String resultadoString = Double.toString(resultado).replace(".", ",");
+		boolean inteiro = resultadoString.endsWith(",0");
+		
+		return inteiro ? resultadoString.replace(",0", "") : resultadoString;
 	}
 
 	private TipoComando detectarTipoComando(String texto) {
